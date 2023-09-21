7c967b75f30b9a72cde4355b8a8a9f900f994c19
aeneasr <3372410+aeneasr@users.noreply.github.com>
2023-09-21 13:51:13 +0200

    chore: synchronize workspaces

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,20 @@
-import { SignIn } from "@clerk/nextjs";
+"use client"
+import {SignIn, useClerk, useSignIn, useSignUp} from "@clerk/nextjs";
+import {useCallback, useEffect} from "react";
 
 export default function Page() {
-  return (
-    <div className="flex justify-center py-24">
-      <SignIn />
-    </div>
-  );
+
+    return (
+        <div className="flex justify-center py-24">
+            <SignIn appearance={
+                {
+                    elements: {
+                        headerTitle: {
+                            // textDecoration: "underline"
+                        }
+                    }
+                }
+            }/>
+        </div>
+    );
 }
